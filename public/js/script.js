33ab257a55e8b92b88e646b1566b540d298eae29
Ryoneme2 <worknarair6@gmail.com>
2022-05-26 18:52:03 +0700

    update responsive

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ const createCardElement = (card, selector) => {
   const recommendSection = document.querySelector(`#${selector}`);
 
   const sectionCard = document.createElement("section");
-  sectionCard.classList.add("flex-shrink-0", "rounded", "h-auto", "w-[15rem]");
+  sectionCard.classList.add("flex-shrink-0", "rounded", "h-auto", "md:w-[15rem]" , "w-[10rem]");
   const cardElem = document.createElement("a");
   cardElem.href = ''
   cardElem.classList.add(
@@ -13,39 +13,42 @@ const createCardElement = (card, selector) => {
     "bg-no-repeat",
     "bg-cover",
     "rounded-xl",
-    "h-[20rem]"
+    "h-[12rem]",
+    "w-[9rem]",
+    "md:h-[20rem]",
+    "md:w-[15rem]",
   );
   cardElem.setAttribute(
     "style",
     `background-image: url(${card.image})`
   );
-  const spanStar = document.createElement("span");
-  spanStar.classList.add(
-    "absolute",
-    "z-10",
-    "inline-flex",
-    "items-center",
-    "px-3,",
-    "py-1",
-    "text-xs",
-    "font-semibold",
-    "text-white",
-    "bg-black",
-    "rounded-full",
-    "left-4",
-    "top-4"
-  );
-  spanStar.innerHTML = `
-  ${card.rating}
-  <svg xmlns="http://www.w3.org/2000/svg"
-      class="h-4 w-4 ml-1.5 text-yellow-300"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-      />
-    </svg>`;
+  // const spanStar = document.createElement("span");
+  // spanStar.classList.add(
+  //   "absolute",
+  //   "z-10",
+  //   "inline-flex",
+  //   "items-center",
+  //   "px-3,",
+  //   "py-1",
+  //   "text-xs",
+  //   "font-semibold",
+  //   "text-white",
+  //   "bg-black",
+  //   "rounded-full",
+  //   "left-4",
+  //   "top-4"
+  // );
+  // spanStar.innerHTML = `
+  // ${card.rating}
+  // <svg xmlns="http://www.w3.org/2000/svg"
+  //     class="h-4 w-4 ml-1.5 text-yellow-300"
+  //     viewBox="0 0 20 20"
+  //     fill="currentColor"
+  //   >
+  //     <path
+  //       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+  //     />
+  //   </svg>`;
   const spanHeart = document.createElement("span");
   spanHeart.classList.add(
     "absolute",
@@ -69,20 +72,23 @@ const createCardElement = (card, selector) => {
   const cardDetail = document.createElement("div");
   cardDetail.classList.add(
     "relative",
-    "p-6",
+    "p-3",
+    "md:p-6",
     "pt-40",
     "text-orange-700",
     "white-gradient",
-    "h-[20rem]",
+    "h-[12rem]",
+    "md:h-[20rem]",
+    "w-full",
     "bg-opacity-40"
   );
   const cardBg = document.createElement("div");
   cardBg.classList.add("absolute", "bottom-4");
-  cardBg.innerHTML = `<h5 class="text-2xl font-bold">${card.title}</h5>`;
+  cardBg.innerHTML = `<h5 class="text-sm md:text-lg lg:text-xl xl:text-2xl font-bold">${card.title}</h5>`;
 
   cardDetail.appendChild(cardBg);
 
-  cardElem.appendChild(spanStar);
+  // cardElem.appendChild(spanStar);
   cardElem.appendChild(spanHeart);
   cardElem.appendChild(cardDetail);
   sectionCard.appendChild(cardElem);
